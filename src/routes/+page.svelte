cabf47f3283671b38df36e986448792e6635d848
Jordan <Jordantay9014@outlook.com>
2024-03-21 15:30:43 -0500

    feat/bt-lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>ML / MLOps Resume - Jordan Taylor</h1>
-<p>Hey, I'm a Senior Platform Engineering at Basistheory, where I've built a solid base in platform, and software engineering foundations. </p>
+<p>Hey, I'm a Senior Platform Engineering at <a href="https://Basistheory.com">Basistheory</a>, where I've built a solid base in platform, and software engineering foundations. </p>
 <p>Along the way, I started diving into the exciting world of Machine Learning (ML) and MLOps. This is my learning in the open so that I am able to showcase my progress into ML.</p>
 
 <br/>
