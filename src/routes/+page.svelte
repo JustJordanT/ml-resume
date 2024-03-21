ee3c857095df6e98302135ccf883f805604124ad
Jordan <Jordantay9014@outlook.com>
2024-03-21 15:09:17 -0500

    feat/updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>ML Resume</h1>
+<p>Hey, I'm a Senior Platform Engineering at Basistheory, where I've built a solid base in platform, and software engineering foundations. </p>
+<p>Along the way, I started diving into the exciting world of Machine Learning (ML) and MLOps. This is my learning in the open so that I am able to showcase my progress into ML.</p>
+
+<br/>
+
+<h2>Posts</h2>
+
+<h2>Projects</h2>
+
+
+
