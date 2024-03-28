0d44c9db21d4ee541bc7c3b5a58c8b3d334f7882
JT <Jordantay9014+master@gmail.com>
2024-03-28 18:39:37 -0500

    feat/udpates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Software Engineering Resume - Jordan Taylor</h1>
-<p>Hey, I'm a Senior Platform Engineering at <a href="https://Basistheory.com">Basistheory</a>, I've built a solid base in platform, and software engineering foundations over the year. </p>
+<p>Hey, I'm a Senior Platform Engineering at <a href="https://Basistheory.com">Basistheory</a>, I've built a solid base in platform, and software engineering foundations over the year, working with C#,Go. </p>
 <p>Currently working on a few side projects that I will be show casing here, tag long with me on this journey.</p>
 <br/>
 
