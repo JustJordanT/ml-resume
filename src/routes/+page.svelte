523b0cc31b7441a9784080a5099057642875f424
Jt <Jordantay9014+master@gmail.com>
2024-05-03 21:11:30 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <h1>Jordan Taylor - Just another software dude</h1>
+
+<a href="https://blog.justjordant.com">Blog</a>
+
+
 <p>Hey, I'm a Senior Platform Engineering at <a href="https://Basistheory.com">Basistheory</a>; I've built a solid base in platform engineering, and software engineering foundations over the years, working with .NET and Go.</p>
 <p>Currently working on a few side projects that I will be show casing here, tag long with me on this journey.</p>
 <br/>
