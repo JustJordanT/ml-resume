cb4e1eebde3d085f66a6a677c36163e227513ea9
Jt <Jordantay9014+master@gmail.com>
2024-06-27 12:31:17 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,9 @@
 <a href="https://blog.justjordant.com">Blog</a>
 
 
-<p>Hey, I'm a Senior Platform Engineering at <a href="https://Basistheory.com">Basistheory</a>; I've built a solid base in platform engineering, and software engineering foundations over the years, working with .NET and Go.</p>
+<p>Hey, I'm a Senior Platform Engineering; I've built a solid base in platform engineering, and software engineering, working with .NET and Go.</p>
 <p>Currently working on a few side projects that I will be show casing here, tag long with me on this journey.</p>
 <br/>
-
-<a href="https://www.linkedin.com/in/justjordant/">LinkenIn</a>
-<a href="https://github.com/JustJordanT">Github</a>
-<a href="https://twitter.com/Just_Jordan_T">Twitter - (X)</a>
 
 <h2>Projects</h2>
 
